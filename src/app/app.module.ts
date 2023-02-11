3a933113f9ae864e926ae780804fed1acf8ac54e
aswinasok97 <124579376+aswinasok97@users.noreply.github.com>
2023-02-11 11:35:57 +0530

    code change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DistrictComponent } from './district/district.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DistrictComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/district/district.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-district',
+  templateUrl: './district.component.html',
+  styleUrls: ['./district.component.css']
+})
+export class DistrictComponent 
+{
+ 
+  database= [
+    {
+      "id":1,
+      "title":"Trivandrum",
+      "details":"Shankumukham Beach",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/ba/95/ee/mermaid.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":2,
+      "title":"Pathanamthitta",
+      "details":"Adavi",
+      "image":"https://img-mm.manoramaonline.com/content/dam/mm/mo/district-news/pathanamthitta/images/2021/9/20/pathanamthitta-thannithodu-adavi-eco-tourism-project.jpg.transform/schema-1x1/image.jpg"
+    },
+    {
+      "id":3,
+      "title":"kollam",
+      "details":"Jadayu Peak",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/9c/f4/af/worlds-largest-bird-sculpture.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":4,
+      "title":"Alappuzha",
+      "details":"Alappey Backwaters",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/cc/95/14/alleppey-backwater-tour.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":5,
+      "title":"Kottayam",
+      "details":"Marmala Waterfall",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/19/ce/79/marmala-waterfall.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":6,
+      "title":"Eranakulam",
+      "details":"Fort Kochi",
+      "image":"https://www.bluebirdtravels.in/wp-content/uploads/uncategorized/Fort-Kochi-Beach-01-scaled-1200x1200-cropped.jpg"
+    },
+    {
+      "id":7,
+      "title":"Idukki",
+      "details":"Anchuruli",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/bd/1f/43/1522484668315-largejpg.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":8,
+      "title":"Thrissur",
+      "details":"Athirappally",
+      "image":"https://img.onmanorama.com/content/dam/mm/en/travel/getting-about-kerala/thrissur/images/2020/10/8/athirappilly-waterfalls.jpg.transform/schema-1x1/image.jpg"
+    },
+    {
+      "id":9,
+      "title":"Malappuram",
+      "details":"Mini Ooty",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/41/2a/8d/cheruppadimala.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":10,
+      "title":"Kozhikkode",
+      "details":"Beppur",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/2f/ee/ad/beypore-beach.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":11,
+      "title":"Palakkad",
+      "details":"Palakkad Fort",
+      "image":"https://img.onmanorama.com/content/dam/mm/en/travel/getting-about-kerala/palakkad/images/2018/10/3/palakkad-fort.jpg.transform/schema-1x1/image.jpg"
+    },
+    {
+      "id":12,
+      "title":"Wayanad",
+      "details":"KaduvaKuzhi View Point",
+      "image":"https://keralatouristplaces.in/wp-content/uploads/2023/01/Kaduvakuzhi-View-Point-Wayanad.webp"
+    },
+    {
+      "id":13,
+      "title":"Kannur",
+      "details":"Ezhimala",
+      "image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ff/f5/b0/ezhimala-viewpoint.jpg?w=1200&h=1200&s=1"
+    },
+    {
+      "id":14,
+      "title":"Kasargod",
+      "details":"Bekkal Fort",
+      "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Bekal_Fort_Kerala.jpg/1200px-Bekal_Fort_Kerala.jpg"
+    },
+
+  ]
+
+}
